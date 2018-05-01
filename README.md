8f7611e7d9bad1c82002b6586d4f1cfb8c64bfef
Simon Auer <SimonErich@gmail.com>
2018-05-01 19:12:05 +0200

    added gesture support

--- README.md
+++ README.md
@@ -6,3 +6,38 @@ A view component for react-native with pinch to zoom, tap to move and double tap
 
 Readme: 
 Coming soon
+
+## Props
+
+### Events
+
+| name | description | params | expected return |
+| ---- | ----------- | ------ | --------------- |
+| onStartShouldSetPanResponder | description | event, gestureState, zoomableViewEventObject, baseComponentResult | {boolean} whether panresponder should be set or not |
+| onMoveShouldSetPanResponder | description | event, gestureState, zoomableViewEventObject, baseComponentResult | {boolean} whether panresponder should be set or not |
+| onPanResponderGrant | description | event, gestureState, zoomableViewEventObject | void |
+| onPanResponderEnd | Will be called when gesture ends | event, gestureState, zoomableViewEventObject | void |
+| onPanResponderMove | Will be called when user moves while touching | event, gestureState, zoomableViewEventObject | void |
+| onDoubleTap | Will be called, when user double taps the screen | event, gestureState, zoomableViewEventObject | void |
+| onShiftingBefore | Will be called, when user taps and moves the view, but before our view movement work kicks in (so this is the place to interrupt movement, if you need to)  | event, gestureState, zoomableViewEventObject |  {boolean} if this returns false, ZoomableView will not process the shift, otherwise it will |
+| onShiftingAfter | Will be called, when user taps and moves the view, but after the values have changed already | event, gestureState, zoomableViewEventObject | void |
+| onShiftingEnd | Will be called, when user stops a tap and move gesture | event, gestureState, zoomableViewEventObject | void |
+| onPinchMovementBefore | Will be called, while the user pinches the screen, but before our zoom work kicks in (so this is the place to interrupt zooming, if you need to) | event, gestureState, zoomableViewEventObject | {boolean} if this returns false, ZoomableView will not process the pinch, otherwise it will |
+| onPinchMovementAfter | Will be called, while the user pinches the screen, but after the values have changed already | event, gestureState, zoomableViewEventObject | {boolean} if this returns false, ZoomableView will not process the pinch, otherwise it will |
+| onPinchEnd | Will be called, when the pinch has ended | event, gestureState, zoomableViewEventObject | {boolean} if this returns false, ZoomableView will not process the pinch, otherwise it will |
+
+
+## zoomableViewEventObject
+
+The zoomableViewEventObject object is attached to every event and represents the current state of our zoomable view.
+```json
+   {
+      zoomLevel: number,         // current level of zooming (usually a value between minZoom and maxZoom)
+      offsetX: number,           // current offset left
+      offsetY: number,           // current offset top
+      lastScale: number,         // last zoom level (before we started the movement)
+      lastX: number,             // last offset left (before we started the movement)
+      lastY: number,             // last offset top (before we started the movement)
+      lastMovePinch: boolean,    // information if a movement is going on
+   }
+```
---
+++ src/ReactNativeZoomableViewWithGestures.js
@@ -0,0 +1,228 @@
+"use strict";
+import React, { Component } from "react";
+import { View, PanResponder } from "react-native";
+import PropTypes from 'prop-types';
+import ReactNativeZoomableView from './ReactNativeZoomableView';
+
+export const swipeDirections = {
+  SWIPE_UP: "SWIPE_UP",
+  SWIPE_DOWN: "SWIPE_DOWN",
+  SWIPE_LEFT: "SWIPE_LEFT",
+  SWIPE_RIGHT: "SWIPE_RIGHT"
+};
+
+class ReactNativeZoomableViewWithGestures extends React.Component {
+  _onShiftingEnd = (e, gestureState, zoomableViewState) => {
+    if (this.props.onShiftingEnd) {
+      this._onShiftingEnd(e, gestureState, zoomableViewState);
+    }
+
+    if (!this._couldCallSwipeEvent) {
+      return;
+    }
+
+    const swipeDirection = this._getSwipeDirection(gestureState);
+    this._triggerSwipeHandlers(swipeDirection, gestureState);
+  }
+
+  /**
+   * Checks if current config options make it possible to process a swipe or if is not necessary
+   *
+   * @returns {*}
+   * @private
+   */
+  _couldCallSwipeEvent(zoomableViewState) {
+    const { onSwipe, onSwipeUp, onSwipeDown, onSwipeLeft, onSwipeRight, swipeMaxZoom, swipeMinZoom } = this.props;
+
+    if (swipeMaxZoom && zoomableViewState.zoomLevel > swipeMaxZoom) {
+      return false;
+    }
+
+    if (swipeMinZoom && zoomableViewState.zoomLevel < swipeMinZoom) {
+      return false;
+    }
+
+    return (onSwipe && onSwipeUp && onSwipeDown && onSwipeLeft && onSwipeRight);
+  }
+
+  /**
+   * Checks the swipe and validates whether we should process it or not
+   *
+   * @param gestureState
+   * @returns {*|boolean}
+   *
+   * @private
+   */
+  _validateSwipe(gestureState) {
+    const {
+      onSwipeUp,
+      onSwipeDown,
+      onSwipeLeft,
+      onSwipeRight
+    } = this.props;
+    const swipeDirection = this._getSwipeDirection(gestureState);
+    const { SWIPE_LEFT, SWIPE_RIGHT, SWIPE_UP, SWIPE_DOWN } = swipeDirections;
+
+    return (
+      (onSwipeUp && swipeDirection === SWIPE_UP) ||
+      (onSwipeDown && swipeDirection === SWIPE_DOWN) ||
+      (onSwipeLeft && swipeDirection === SWIPE_LEFT) ||
+      (onSwipeRight && swipeDirection === SWIPE_RIGHT)
+    );
+  }
+
+  /**
+   * Triggers the correct directional callback
+   *
+   * @param swipeDirection
+   * @param gestureState
+   * @private
+   */
+  _triggerSwipeHandlers(swipeDirection, gestureState) {
+    const {
+      onSwipe,
+      onSwipeUp,
+      onSwipeDown,
+      onSwipeLeft,
+      onSwipeRight
+    } = this.props;
+    const { SWIPE_LEFT, SWIPE_RIGHT, SWIPE_UP, SWIPE_DOWN } = swipeDirections;
+
+    // trigger the general onswipe callback
+    if (onSwipe) {
+      onSwipe(swipeDirection, gestureState);
+    }
+
+    // trigger the direction specific swipe callback
+    switch (swipeDirection) {
+      case SWIPE_LEFT:
+        onSwipeLeft && onSwipeLeft(gestureState);
+        break;
+      case SWIPE_RIGHT:
+        onSwipeRight && onSwipeRight(gestureState);
+        break;
+      case SWIPE_UP:
+        onSwipeUp && onSwipeUp(gestureState);
+        break;
+      case SWIPE_DOWN:
+        onSwipeDown && onSwipeDown(gestureState);
+        break;
+    }
+  }
+
+  /**
+   * Calculates what direction the user swiped
+   *
+   * @param gestureState
+   * @returns {*}
+   * @private
+   */
+  _getSwipeDirection(gestureState) {
+    const { swipeLengthThreshold } = this.props;
+    const { SWIPE_LEFT, SWIPE_RIGHT, SWIPE_UP, SWIPE_DOWN } = swipeDirections;
+    const { dx, dy } = gestureState;
+
+    if (this._isValidHorizontalSwipe(gestureState)) {
+      if (Math.abs(dx) > swipeLengthThreshold) {
+        return dx > 0 ? SWIPE_RIGHT : SWIPE_LEFT;
+      }
+    } else if (this._isValidVerticalSwipe(gestureState)) {
+      if (Math.abs(dy) > swipeLengthThreshold) {
+        return dy > 0 ? SWIPE_DOWN : SWIPE_UP;
+      }
+    }
+
+    return null;
+  }
+
+  /**
+   * Checks, whether the swipe was done in a horizontal fashion, respecting swipeVelocityThreshold limits
+   *
+   * @param gestureState
+   * @returns {*}
+   *
+   * @private
+   */
+  _isValidHorizontalSwipe(gestureState) {
+    const { vx, dy } = gestureState;
+    const { swipeVelocityThreshold, swipeDirectionalThreshold } = this.props;
+    return this._isValidSwipe(vx, swipeVelocityThreshold, dy, swipeDirectionalThreshold);
+  }
+
+  /**
+   * Checks, whether the swipe was done in a vertical fashion, respecting swipeVelocityThreshold limits
+   *
+   * @param gestureState
+   * @returns {*}
+   *
+   * @private
+   */
+  _isValidVerticalSwipe(gestureState) {
+    const { vy, dx } = gestureState;
+    const { swipeVelocityThreshold, swipeDirectionalThreshold } = this.props;
+    return this._isValidSwipe(vy, swipeVelocityThreshold, dx, swipeDirectionalThreshold);
+  }
+
+  /**
+   * Checks the sipw against velocity and directional offset to make sure it only gets activated, when we actually need it
+   *
+   * @param velocity
+   * @param swipeVelocityThreshold
+   * @param directionalOffset
+   * @param swipeDirectionalThreshold
+   *
+   * @returns {boolean}
+   *
+   * @private
+   */
+  _isValidSwipe(
+    velocity,
+    swipeVelocityThreshold,
+    directionalOffset,
+    swipeDirectionalThreshold
+  ) {
+    return (
+      Math.abs(velocity) > swipeVelocityThreshold &&
+      Math.abs(directionalOffset) < swipeDirectionalThreshold
+    );
+  }
+
+  render() {
+    return (
+      <ReactNativeZoomableView
+        {...this.props}
+        onShiftingEnd={this._onShiftingEnd}
+      />
+    )
+  }
+}
+
+ReactNativeZoomableViewWithGestures.propTypes = {
+  swipeLengthThreshold: PropTypes.number,
+  swipeVelocityThreshold: PropTypes.number,
+  swipeDirectionalThreshold: PropTypes.number,
+  swipeMinZoom: PropTypes.number,
+  swipeMaxZoom: PropTypes.number,
+  swipeDisabled: PropTypes.bool,
+  onSwipe: PropTypes.func,
+  onSwipeUp: PropTypes.func,
+  onSwipeDown: PropTypes.func,
+  onSwipeLeft: PropTypes.func,
+  onSwipeRight: PropTypes.func,
+}
+
+ReactNativeZoomableViewWithGestures.defaultProps = {
+  swipeLengthThreshold: 0,
+  swipeVelocityThreshold: 0.1,
+  swipeDirectionalThreshold: 120,
+  swipeMinZoom: null,
+  swipeMaxZoom: null,
+  swipeDisabled: false,
+  onSwipe: null,
+  onSwipeUp: null,
+  onSwipeDown: null,
+  onSwipeLeft: null,
+  onSwipeRight: null,
+}
+
+export default ReactNativeZoomableViewWithGestures;
