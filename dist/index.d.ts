f1872f0cba80a08e26701d4c2252bb474a8d139a
Simon Auer <simon@du.digital>
2021-06-11 07:25:59 +0200

    remove dist folder

--- dist/index.d.ts
+++
@@ -1,3 +0,0 @@
-import ReactNativeZoomableView from './ReactNativeZoomableView';
-import ReactNativeZoomableViewWithGestures from './ReactNativeZoomableViewWithGestures';
-export { ReactNativeZoomableView, ReactNativeZoomableViewWithGestures };
