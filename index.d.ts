16f347b0191b7d5ab840ea762fd2efbc7f60ea63
thomasvo <thomasttvo@gmail.com>
2021-09-21 16:28:40 -0700

    Exported some types

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,13 @@
 import ReactNativeZoomableView from './src/ReactNativeZoomableView';
 import ReactNativeZoomableViewWithGestures from './src/ReactNativeZoomableViewWithGestures';
+import {
+  ReactNativeZoomableViewProps,
+  ZoomableViewEvent,
+} from '@openspacelabs/react-native-zoomable-view';
 
-export { ReactNativeZoomableView, ReactNativeZoomableViewWithGestures };
+export {
+  ReactNativeZoomableView,
+  ReactNativeZoomableViewWithGestures,
+  ReactNativeZoomableViewProps,
+  ZoomableViewEvent,
+};
