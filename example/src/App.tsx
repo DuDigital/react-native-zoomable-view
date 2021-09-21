698f323d57a5fb6365c2dcda7803aa605a61c829
thomasvo <thomasttvo@gmail.com>
2021-09-21 15:42:59 -0700

    Added logic to show the example

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,18 +1,29 @@
 import * as React from 'react';
 
-import { StyleSheet, View, Text } from 'react-native';
-import ReactNativeZoomableView from '@openspacelabs/react-native-zoomable-view';
+import { StyleSheet, View, Text, Image } from 'react-native';
+import { ReactNativeZoomableView } from '@openspacelabs/react-native-zoomable-view';
 
 export default function App() {
-  const [result, setResult] = React.useState<number | undefined>();
-
-  React.useEffect(() => {
-    ReactNativeZoomableView.multiply(3, 7).then(setResult);
-  }, []);
-
   return (
     <View style={styles.container}>
-      <Text>Result: {result}</Text>
+      <Text>ReactNativeZoomableView</Text>
+      <View style={{ borderWidth: 5, flexShrink: 1, height: 500, width: 310 }}>
+        <ReactNativeZoomableView
+          maxZoom={30}
+          // Give these to the zoomable view so it can apply the boundaries around the actual content.
+          // Need to make sure the content is actually centered and the width and height are
+          // dimensions when it's rendered naturally. Not the intrinsic size.
+          // For example, an image with an intrinsic size of 400x200 will be rendered as 300x150 in this case.
+          // Therefore, we'll feed the zoomable view the 300x100 size.
+          contentWidth={300}
+          contentHeight={150}
+        >
+          <Image
+            style={{ width: '100%', height: '100%', resizeMode: 'contain' }}
+            source={{ uri: 'https://via.placeholder.com/400x200.png' }}
+          />
+        </ReactNativeZoomableView>
+      </View>
     </View>
   );
 }
@@ -22,6 +33,7 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 20,
   },
   box: {
     width: 60,
